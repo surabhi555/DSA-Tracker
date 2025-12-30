20c911dd8bac4920d744015e263ac09de10bdba3
surabhi555 <99637606+surabhi555@users.noreply.github.com>
2025-12-30 19:18:51 +0530

    day 4 done!

--- Tree/checkIfTwoTreesAreIdentical.cpp
+++ Tree/checkIfTwoTreesAreIdentical.cpp
@@ -1,3 +1,4 @@
+//recursive approch
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -11,5 +12,47 @@ public:
             return false;
         }
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
+//BFS 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL){
+            return true;
+        }
+        if(p == NULL || q ==NULL){
+            return false;
+        }
+        queue<TreeNode*>q1;
+        queue<TreeNode*>q2;
+        q1.push(p);
+        q2.push(q);
+    while(!q1.empty() && !q2.empty()){
+        TreeNode* node1 =q1.front();
+        q1.pop();
+        TreeNode* node2 =q2.front();
+        q2.pop();
+        if(node1->val != node2->val){
+            return false;
+        }
+        if(node1->left != NULL && node2->left != NULL){
+            q1.push(node1->left);
+            q2.push(node2->left);
+
+        }
+        else if(node1 ->left != NULL || node2->left!= NULL){
+            return false;
+        }
+        if(node1->right != NULL && node2->right != NULL){
+            q1.push(node1->right);
+            q2.push(node2->right);
+
+        }
+        else if(node1 ->right != NULL || node2->right!= NULL){
+            return false;
+        }
+    }
+    return true;
     }
 };
